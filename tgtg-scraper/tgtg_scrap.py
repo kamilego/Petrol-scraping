0c85cce818d803a9e426d079af904a576de3cb1e
Kamil <80158455+kamilego@users.noreply.github.com>
2022-09-24 22:28:20 +0200

    added town and distance
    script requests town and distance information
    shows hour of scaning

--- tgtg-scraper/tgtg_scrap.py
+++ tgtg-scraper/tgtg_scrap.py
@@ -1,22 +1,31 @@
 import sys
 import winsound
 import time
+from datetime import datetime
 from tgtg import TgtgClient
+from geopy.geocoders import Nominatim
 
 
 """
 run using sys
-$ py tgtg_scrap.py john.smith@outlook.com
+$ py tgtg_scrap.py john.smith@example.com Warszawa 30
+mail, town, distance
 """
 
 
 email = sys.argv[1]
+location = sys.argv[2]
+distance = int(sys.argv[3])
 client = TgtgClient(email=email)
 credentials = client.get_credentials()
 access_token = credentials["access_token"]
 refresh_token = credentials["refresh_token"]
 user_id = credentials["user_id"]
-client = TgtgClient(access_token=access_token, refresh_token=refresh_token, user_id=user_id)
+client = TgtgClient(access_token=access_token,
+                    refresh_token=refresh_token,
+                    user_id=user_id)
+loc = Nominatim(user_agent="GetLoc")
+getLoc = loc.geocode(location)
 
 
 def play_sound() -> None:
@@ -27,14 +36,18 @@ def play_sound() -> None:
 
 def main() -> None:
     while True:
-        items = client.get_items()
-        found = {elem["display_name"]: elem["items_available"] for elem in items if elem["items_available"] != 0}
+        items = client.get_items(
+            latitude=getLoc.latitude,
+            longitude=getLoc.longitude,
+            page_size=100)
+        found = {elem["display_name"]: elem["items_available"] for elem in items if elem["items_available"] != 0 and elem["distance"] < distance}
+        cur_time = datetime.now().strftime("%H:%M:%S")
         if found:
             play_sound()
             for name, amount in found.items():
-                print(f"{name} - amount: {amount}")
+                print(f"{cur_time} - {name} - amount: {amount}")
         else:
-            print("Nothing")
+            print(f"{cur_time} - Nothing")
         time.sleep(10)
 
 
