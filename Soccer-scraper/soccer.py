8f7ae31b64fb35c47cb40e8c55518097fad9af99
kamilego <kam.legowicz@gmail.com>
2023-10-01 17:52:11 +0200

    scripting all pinned matches

--- Soccer-scraper/soccer.py
+++ Soccer-scraper/soccer.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
 import time
+import sys
 
+"""
+py soccer.py 5
+"""
+
+last_matches = int(sys.argv[1])
 website_url = r"https://www.flashscore.com/"
-
 chromedriver_path = r"C:\Users\default.DESKTOP-E4TLVMN\Downloads\chromedriver.exe"
+
 service = Service(chromedriver_path)
 options = webdriver.ChromeOptions()
 # options.add_argument("headless")
@@ -42,7 +46,33 @@ for num, elem in enumerate(events):
     else:
         events_dict[soccer_league].append(elem)
 
-for key, value in events_dict.items():
-    print(key)
-    for v in value:
-        print(v.text)
+print()
+for league, matches in events_dict.items():
+    print(league)
+    print("-"*30)
+    for match in matches:
+        time.sleep(1)
+        match.click()
+        time.sleep(1)
+        new_window = driver.window_handles
+        driver.switch_to.window(new_window[1])
+        tabs = driver.find_elements(By.CLASS_NAME, "tab__tab")
+        for num, tab in enumerate(tabs):
+            tab_text = tab.get_attribute('innerHTML')
+            if 'H2H' in tab_text:
+                tab.click()
+        time.sleep(2)
+        groups = driver.find_elements(By.CLASS_NAME, "h2h__section")
+        for group in groups:
+            team_title = group.find_element(By.CLASS_NAME, "section__title").text
+            results = group.find_elements(By.CLASS_NAME, "h2h__result")[:last_matches]
+            print(team_title)
+            goal_sum = 0
+            for match in results:
+                goals = [int(elem) for elem in match.text.split("\n")]
+                goal_sum += sum(goals)
+            print(f"AVG goals: {goal_sum/last_matches} in last 5 games.")
+        driver.close()
+        driver.switch_to.window(new_window[0])
+        print("-"*5)
+driver.close()
